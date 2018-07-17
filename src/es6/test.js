8791154df8bcbf79437f1853748274d737e7fefa
cyrsis <em3888@gmail.com>
2018-07-17 12:37:51 +0800

    sdfsdf

--- src/es6/test.js
+++ src/es6/test.js
@@ -1,19 +1,25 @@
+// const source = [1, 5, 9, 3, 'hi', 'tb', 456, '11', 'yoyoyo'];
+// let total = 0;
+//
+// for (let i = 0; i < source.length; i++) {
+//     let num = parseInt(source[i], 10);
+//     if (!isNaN(num)) {
+//         total += num * 2;
+//     }
+// }
+
+
 const source = [1, 5, 9, 3, 'hi', 'tb', 456, '11', 'yoyoyo'];
-let total = 0;
 
-for (let i = 0; i < source.length; i++) {
-    let num = parseInt(source[i], 10);
-    if (!isNaN(num)) {
-        total += num * 2;
-    }
-}
+// let total = source
+//     .map(x => parseInt(x, 10))
+//     .filter(x => !isNaN(x))
+//     .map(x => x * 2)
+//     .reduce((total, value) => total + value )
 
 
-const source = [1, 5, 9, 3, 'hi', 'tb', 456, '11', 'yoyoyo'];
+let total = source.map((x) => parseInt(x,10)).filter((x) => !isNaN(x)).map((x) => x*2).reduce((total , value) => total+value)
 
-let total = source
-    .map(x => parseInt(x, 10))
-    .filter(x => !isNaN(x))
-    .map(x => x * 2)
-    .reduce((total, value) => total + value )
+console.log(total)
+
 
