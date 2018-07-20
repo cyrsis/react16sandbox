cdae767ce8e9250a20979854ac37a4611214a17a
Victor <em3888@gmail.com>
2018-07-20 09:43:00 +0800

    Clean up the mess I have

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ import { Person } from "./Component/Person";
 
 class App extends Component {
 
-    state = {
-        person: [
-            {name: "Max", age: "25"},
-            {name: "Victor", age: "35"},
-            {name: "Mary", age: "37"},
-        ],
-        otherstate: "Something else",
-        number: 0
-    }
+    state = {}
 
 
     render() {
@@ -23,31 +15,11 @@ class App extends Component {
             <div>
 
 
-                <Person name={this.state.person[0].name} age={this.state.person[0].age}></Person>
-                <Person name={this.state.person[1].name} age={this.state.person[1].age}
-                        click={this.switchNameHandler}></Person>
-                <Person name={this.state.person[2].name} age={this.state.person[2].age}></Person>
-
-                <button onClick={this.switchNameHandler}>Switch name</button>
-
-                <div>{this.state.number} -</div>
             </div>
         );
     }
 
-    switchNameHandler = () => {
 
-        this.setState({
-            person: [
-                {name: "Maxium Salve", age: "25"},
-                {name: "Victor", age: "35"},
-                {name: "Mary", age: "37"},
-            ]
-
-
-        })
-
-    }
 }
 
 export default App;
